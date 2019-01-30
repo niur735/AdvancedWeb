50df68b3c9e414f03a20ac4de810f784abe1a5e8
Verity <verity0821@yahoo.com>
2019-01-30 17:06:06 -0500

    first edit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(".change").click(function() {
+    $(".introscreen").css('display','none');
+    $(".background").removeClass('unseen');
+    $(".workscreen").removeClass('unseen');
+    $(".pic").removeClass('unseen');
+});
