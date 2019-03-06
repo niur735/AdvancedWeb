78f1fa2d22058bfd236a5b32fe8cdf510760d4ca
niur735 <31444265+niur735@users.noreply.github.com>
2019-03-06 13:40:35 -0500

    draft1

--- web calculator/js/script.js
+++ web calculator/js/script.js
@@ -1,24 +1,63 @@
-document.getElementById("one").innerHTML = "Hi I am the first one";
-document.getElementsByTagName("p")[0].innerHTML = "YEAH";
-document.querySelector("div").innerHTML = "yoooooo";
-document.querySelector(".class").innerHTML = "time to go babe";
+$(function() {
 
-// function clickme(){
-// 	alert("YES!");
-// };
-
-
-// function enter(){
-// var sum = Number(getElementById("n1").value) + Number(getElementById("n2").value);
-
-// console.log(sum);
-// };
+		var empty = true;
+		$("input").each(function(){
+		   if($(this).val()!=""){
+		      empty = false;
+		      return false;
+		    }
+		 });
 
 
-function enterNow(){
-var n1 = document.getElementById("n1").value;
-var n2 = document.getElementById("n2").value;
+	function listCreator (){
+		let number = $('#cnumber').val();
+		for (var i = 0; i < number; i++) {
+				//add form elenents
+		
+		$('.customerlist').append('<div class="list">Customer<div class="order"></div><div class="check">Check</div></div>');}	
 
- var sum = Number(n1) + Number(n2);
- console.log(sum);
-};
+		var clicked = false;
+
+		$('.order').click(function(){
+ 			 if (clicked == false) {
+ 			 $(this).addClass('background');
+ 			 clicked = true; }
+ 			 
+ 			 else{
+ 			 $(this).removeClass('background');
+ 			 clicked = false;}
+ 			 });
+	};
+
+	function addItem (){
+		$('.item').click(function(){
+			let item = $(this).data('item');
+			let price = $(this).data('amount');
+			var orderdetial = '<li class="orderitem">'+ item +'</li><div class="moneysign">$<div value="'+ price +'" class="orderprice">'+ price +'</div></div>';
+			$('.background').append(orderdetial); 
+		});
+
+		$('.check').click(function(){
+			var price = $('.orderprice').val();
+			alert(price);
+// how to get the first val from multiple class with same name?
+
+		});
+	};
+	
+	$("#cnumber").keypress(function(event) {
+			    if (event.keyCode==13) {
+			    	listCreator()
+			    	addItem()
+			    	$('.add').css('display','none');}	
+	});
+				
+	$("#myBtn").click(function(event){ 
+			        //send to function
+			        listCreator()
+			        addItem()
+			        $('.add').css('display','none');    
+	});
+});
+
+
