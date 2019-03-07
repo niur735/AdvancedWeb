28a45afcf46f8b689f3102d733fc0116faef1abb
niur735 <31444265+niur735@users.noreply.github.com>
2019-03-06 23:11:40 -0500

    draft2

--- web calculator/js/script.js
+++ web calculator/js/script.js
@@ -14,11 +14,18 @@ $(function() {
 		for (var i = 0; i < number; i++) {
 				//add form elenents
 		
-		$('.customerlist').append('<div class="list">Customer<div class="order"></div><div class="check">Check</div></div>');}	
+		$('.customerlist').append('<div class="list">Customer<div class="order"><div class="orderlist"></div><div class="cal"><div class="subtotal"></div><div class="tips"></div><div class="tax"></div><div class="total"></div></div></div></div>');}	
+		changeStatu();
+		$('.subtotal').html('Subtotal:$0');
+		$('.tips').html('Tips:$0');
+		$('.tax').html('Tax:$0');
+		$('.total').html('Total:$0');
+	};
 
-		var clicked = false;
+	function changeStatu (){
+			var clicked = false;
 
-		$('.order').click(function(){
+			 $('.order').click(function(){
  			 if (clicked == false) {
  			 $(this).addClass('background');
  			 clicked = true; }
@@ -27,37 +34,61 @@ $(function() {
  			 $(this).removeClass('background');
  			 clicked = false;}
  			 });
-	};
-
-	function addItem (){
-		$('.item').click(function(){
-			let item = $(this).data('item');
-			let price = $(this).data('amount');
-			var orderdetial = '<li class="orderitem">'+ item +'</li><div class="moneysign">$<div value="'+ price +'" class="orderprice">'+ price +'</div></div>';
-			$('.background').append(orderdetial); 
-		});
-
-		$('.check').click(function(){
-			var price = $('.orderprice').val();
-			alert(price);
-// how to get the first val from multiple class with same name?
-
-		});
-	};
+	};	
+		
 	
+	function addItem (){
+		var subtotal = 0;
+		var tips = 0;
+		var tax = 0;
+		var total = 0;
+
+		$('.item').click(function(){
+			var item = $(this).data('item');
+			var price = $(this).data('amount');
+			var orderdetial = '<li class="orderitem">'+ item +'</li><div class="moneysign">$<div data-price="'+ price +'" class="orderprice">'+ price +'</div></div>';
+			$('.orderlist').append(orderdetial); 
+			subtotal = (subtotal + price);
+			tips = (subtotal * 0.18).toFixed(2);
+			tax = (subtotal * 0.085).toFixed(2);
+			total = (subtotal + tips + tax);
+   			$('.subtotal').html('Subtotal:$' + subtotal);
+   			$('.tips').html('Tips:$' + tips);
+
+   			// question part
+   			$('.tas').html('Tax:$' + tax);
+   			$('.total').html('Total:$' + total);
+   			// question part
+		});			
+	};
+
+
+
+
 	$("#cnumber").keypress(function(event) {
 			    if (event.keyCode==13) {
 			    	listCreator()
 			    	addItem()
-			    	$('.add').css('display','none');}	
+			    	$('.add').css('display','none');
+			    	$('.guide2').css('display','block');}	
 	});
 				
 	$("#myBtn").click(function(event){ 
 			        //send to function
 			        listCreator()
 			        addItem()
-			        $('.add').css('display','none');    
+			        $('.add').css('display','none');  
+			        $('.guide2').css('display','block');
 	});
+
 });
+
+
+
+		
+// how to get the first val from multiple class with same name
+	
+	
+
 
 
